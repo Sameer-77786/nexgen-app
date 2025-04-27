c9e6e92fc68ebb0037fdb9b6d5afe57d4961ac59
Sam <sameerspam0786@gmail.com>
2025-04-27 19:24:07 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.nexgen.nexgenapp',
+  appName: 'nexgen-app',
+  webDir: 'build'
+};
+
+export default config;
